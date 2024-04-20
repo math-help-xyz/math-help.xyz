1c74e8e80ee1bbad3eb8f76b22cbc9f217b2bb0c
Lufthansaaa <jayzadev@gmail.com>
2024-04-19 20:18:03 -0700

    first commit of all files

---
+++ socials/script.js
@@ -0,0 +1,11 @@
+const wafflesLike = document.getElementById('wafflesLike');
+const seconds = document.getElementById('seconds');
+const minutes = document.getElementById('minutes');
+
+function yes() {
+    wafflesLike.textContent = "Nice! I like waffles too! 🥞"
+}
+
+function no() {
+    wafflesLike.textContent = "Oh! That's okay, they're not everyone's favorite!🥞"
+}
